0c3ef487ec1cfe01d749fc51216380cdddc03ffd
Danko Caboski <caboskidanko@gmail.com>
2024-10-09 21:07:00 -0300

    Feat: Thread

---
+++ threading .py
@@ -0,0 +1,31 @@
+from threading import Thread
+import random
+
+def soma(amostra: list, origem):
+    s = 0
+    for n in amostra:
+        s = s + n
+    print("Soma: " + origem + " " + str(s)) 
+
+amostraA = random.sample(range(1000000),100000)
+amostraB = random.sample(range(1000000),10)
+
+#Primeira rodada
+print("Primeira rodada de processamento, threads livres")
+tA = Thread(target=soma, args=(amostraA,"A",))
+tB = Thread(target=soma, args=(amostraB,"B",))
+tA.start()
+tB.start()
+
+#Segunda rodada
+print("Segunda rodada de processamento, join limitará a exec")
+tA = Thread(target=soma, args=(amostraA,"A",))
+tB = Thread(target=soma, args=(amostraB,"B",))
+tA.start()
+tB.start()
+tA.join()
+tB.join()
+
+
+
+
